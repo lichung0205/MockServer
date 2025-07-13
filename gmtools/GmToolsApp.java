4b324a8dcb543bbc1b9fb0237820b0f7f48ac4c7
lichung <lichung0205@gmail.com>
2025-07-14 00:04:56 +0800

    建立 gmtools / server / student 三模組目錄基本環境與說明

---
+++ gmtools/GmToolsApp.java
@@ -0,0 +1,7 @@
+package gmtools;
+
+public class GmToolsApp {
+    public static void main(String[] args) {
+        System.out.println("GMTools 已啟動！");
+    }
+}
---
+++ student/StudentApp.java
@@ -0,0 +1,7 @@
+package student;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        System.out.println("學生端連線準備就緒！");
+    }
+}
